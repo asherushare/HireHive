c898798d7bddf68a5d8013414f3671893c12d1e6
Rohitseek <rohit8seek@gmail.com>
2025-09-02 23:41:19 +0530

    location search baar alignment

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,21 +5,22 @@ const Hero = () => {
   return (
     <div className='container 2xl:px-20 mx-auto my-10'>
       <div className='bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl'>
-        <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Over 1 cr randwa sachin</h2>
+        <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Over 10,000+ jobs to apply</h2>
         <p className='mb-8 max-w-xl mx-auto text-sm font-light px-5'>
           Your Next Big Career Move Starts Right here - Explore the Best Job
           Opportunities and Take the First Step Toward Your Future{" "}
         </p>
         <div className='flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4 mx-4 sm:mx-auto'>
-          <div>
+          <div className='flex'>
             <img src={assets.search_icon} alt="" />
             <input
               type="text"
               placeholder="Search for jobs"
               className="max-sm:text-xs p-2 rounded outline-none w-full"
             />
-          </div>
-          <div>
+          </div> 
+          <p className='flex justify-between m items-center'>|</p>
+          <div className='flex'>
             <img src={assets.location_icon} alt="" />
             <input
               type="text"
@@ -27,7 +28,7 @@ const Hero = () => {
               className="max-sm:text-xs p-2 rounded outline-none w-full"
             />
           </div>
-          <button>Search</button>
+          <button className='mr-5'>Search</button>
         </div>
       </div>
     </div>
