da2985c6c4bce786767e91ff0f40e72d63a66bbb
asherushare <asherushare@gmail.com>
2025-09-02 14:24:38 +0530

    first commit

---
+++ client/src/pages/Application.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Application = () => {
+  return (
+    <div>
+      <h1>Application Page</h1>
+      <p>
+        This is the application page where users can apply for jobs.
+      </p>
+    </div>
+  )
+}
+
+export default Application
---
+++ client/src/pages/ApplyJob.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ApplyJob = () => {
+  return (
+    <div>
+      <h1>Apply for a Job</h1>
+      <p>
+        This is the page where users can apply for a job. Please fill out the form below to submit your application.
+      </p>
+    </div>
+  )
+}
+
+export default ApplyJob
