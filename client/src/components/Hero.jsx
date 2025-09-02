0e99688252cb760e3aa9efc2e1a4a1d4315422ea
Rohitseek <rohit8seek@gmail.com>
2025-09-02 14:44:17 +0530

    SOS

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='container 2xl:px-20 mx-auto my-10'>
       <div className='bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl'>
-        <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Over 10,000+ Jobs to apply</h2>
+        <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Over 1 cr randwa sachin</h2>
         <p className='mb-8 max-w-xl mx-auto text-sm font-light px-5'>
           Your Next Big Career Move Starts Right here - Explore the Best Job
           Opportunities and Take the First Step Toward Your Future{" "}
