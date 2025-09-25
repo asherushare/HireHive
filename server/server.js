df50c8ec72725bb57e38f4b3a6f10e9a5ad4825b
Rohitseek <rohit8seek@gmail.com>
2025-09-25 22:50:18 +0530

    Backend Initialization, till Connect DB & Sentry

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import './config/instrument.js'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/db.js';
+
+//initialize Express
+const app = express();
+
+//Connect to database
+//await connectDB();   // commented out to avoid warning in terminal, when we'll  mongo uri then uncomment this line.
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.get('/', (req, res) => res.send('API is running...'));
+
+//Define PORT
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
